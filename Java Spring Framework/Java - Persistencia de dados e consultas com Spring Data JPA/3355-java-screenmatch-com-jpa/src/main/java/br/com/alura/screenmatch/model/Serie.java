4fcb60f7eea8629a4ac13ed95919688c9d97ef05
WhiteArct1c <matrodrigues1576@gmail.com>
2024-07-05 10:23:05 -0300

    feat: add episodes into serie

--- Java Spring Framework/Java - Persistencia de dados e consultas com Spring Data JPA/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ Java Spring Framework/Java - Persistencia de dados e consultas com Spring Data JPA/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -24,7 +24,7 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {
