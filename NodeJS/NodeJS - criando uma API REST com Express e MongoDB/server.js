c4c5d8eb3c6330f0956ee52526449d2938b68d58
WhiteArct1c <matrodrigues1576@gmail.com>
2024-07-24 11:09:24 -0300

    feat: add express framework and build routes for api

--- NodeJS/NodeJS - criando uma API REST com Express e MongoDB/server.js
+++ NodeJS/NodeJS - criando uma API REST com Express e MongoDB/server.js
@@ -1,27 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-   "/": (req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello World\n");
-   },
-   "/users": (req, res) => {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ users: ["João", "Maria", "José"] }));
-   },
-};
-
-const server = http.createServer((req, res) => {
-   if (rotas[req.url]) {
-      rotas[req.url](req, res);
-   } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Not Found\n");
-   }
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
    console.log(`Server running at http://localhost:${PORT}/`);
 });
