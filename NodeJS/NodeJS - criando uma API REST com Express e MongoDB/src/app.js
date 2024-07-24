f96e92026caccfbf327390370f28eded333b0268
WhiteArct1c <matrodrigues1576@gmail.com>
2024-07-24 11:43:32 -0300

    refactor: get books from mongodb

--- NodeJS/NodeJS - criando uma API REST com Express e MongoDB/src/app.js
+++ NodeJS/NodeJS - criando uma API REST com Express e MongoDB/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectaNaDb from "./config/dbconnect.js";
+import livro from "./model/Livro.js";
 
 const conexao = await conectaNaDb();
 
@@ -14,29 +15,14 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json()); //middleware para conversao
 
-const livros = [
-   {
-      id: 1,
-      titulo: "O Senhor dos Anéis"
-   },
-   {
-      id: 2,
-      titulo: "Harry Potter"
-   }
-]
-
-function buscaLivro(id){
-   return livros.findIndex(livro => {
-      return livro.id === Number(id);
-   });
-}
 
 app.get("/", (req, res) => {
    res.status(200).send("Curso de NodeJS");
 });
 
-app.get("/livros", (req, res) => {
-   res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+   const listaLivros = await livro.find({});
+   res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
