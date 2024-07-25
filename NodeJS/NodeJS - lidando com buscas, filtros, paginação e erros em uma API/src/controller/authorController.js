c351a2099e26fa3a92f6fcf5f419f177a669f3f8
WhiteArct1c <matrodrigues1576@gmail.com>
2024-07-25 10:37:57 -0300

    refactor: adjusting http not found code

--- NodeJS/NodeJS - lidando com buscas, filtros, paginação e erros em uma API/src/controller/authorController.js
+++ NodeJS/NodeJS - lidando com buscas, filtros, paginação e erros em uma API/src/controller/authorController.js
@@ -31,8 +31,8 @@ class AuthorController {
       res.status(200).json(author);
     } catch (error) {
       res
-        .status(400)
-        .json({ message: `${error.message} - Id do autor não localizado` });
+        .status(404)
+        .json({ message: `${error.message} - Autor não localizado por Id` });
     }
   }
 
